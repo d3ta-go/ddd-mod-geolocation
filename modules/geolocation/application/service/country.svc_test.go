cbd20d164b878798f038162f968f3c25069bdb03
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-11-15 22:12:36 +0700

    app.layer: refactor test (TDD, unit test)

--- modules/geolocation/application/service/country.svc_test.go
+++ modules/geolocation/application/service/country.svc_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	appDTO "github.com/d3ta-go/ddd-mod-geolocation/modules/geolocation/application/dto/country"
@@ -9,14 +10,18 @@ import (
 	"github.com/d3ta-go/system/system/handler"
 	"github.com/d3ta-go/system/system/identity"
 	"github.com/d3ta-go/system/system/initialize"
+	"github.com/spf13/viper"
 )
 
-func newConfig(t *testing.T) (*config.Config, error) {
-	c, _, err := config.NewConfig("../../../../conf")
+func newConfig(t *testing.T) (*config.Config, *viper.Viper, error) {
+	c, v, err := config.NewConfig("../../../../conf")
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return c, nil
+	if !c.CanRunTest() {
+		panic(fmt.Sprintf("Cannot Run Test on env `%s`, allowed: %v", c.Environment.Stage, c.Environment.RunTestEnvironment))
+	}
+	return c, v, nil
 }
 
 func newCountrySvc(t *testing.T) (*CountrySvc, *handler.Handler, error) {
@@ -25,12 +30,22 @@ func newCountrySvc(t *testing.T) (*CountrySvc, *handler.Handler, error) {
 		return nil, nil, err
 	}
 
-	c, err := newConfig(t)
+	c, v, err := newConfig(t)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	h.SetDefaultConfig(c)
+	h.SetViper("config", v)
+
+	// viper for test-data
+	viperTest := viper.New()
+	viperTest.SetConfigType("yaml")
+	viperTest.SetConfigName("test-data")
+	viperTest.AddConfigPath("../../../../conf/data")
+	viperTest.ReadInConfig()
+	h.SetViper("test-data", viperTest)
+
 	if err := initialize.LoadAllDatabaseConnection(h); err != nil {
 		return nil, nil, err
 	}
@@ -93,8 +108,14 @@ func TestCountrySvc_RefreshIndexer(t *testing.T) {
 		t.Errorf("newCountrySvc: %s", err.Error())
 	}
 
+	viper, err := h.GetViper("test-data")
+	if err != nil {
+		t.Errorf("GetViper: %s", err.Error())
+	}
+	testData := viper.GetStringMapString("test-data.geo-location.country.app-layer.service.refresh-indexer")
+
 	req := new(appDTO.RefreshCountryIndexerReqDTO)
-	req.ProcessType = "SYNC"
+	req.ProcessType = testData["processing-type"]
 
 	i := newIdentity(h, t)
 	i.RequestInfo.RequestObject = "/api/v1/geolocation/countries/indexer/refresh"
@@ -116,8 +137,14 @@ func TestCountrySvc_SearchIndexer(t *testing.T) {
 		t.Errorf("newCountrySvc: %s", err.Error())
 	}
 
+	viper, err := h.GetViper("test-data")
+	if err != nil {
+		t.Errorf("GetViper: %s", err.Error())
+	}
+	testData := viper.GetStringMapString("test-data.geo-location.country.app-layer.service.search-indexer")
+
 	req := new(appDTO.SearchCountryIndexerReqDTO)
-	req.Name = "IN"
+	req.Name = testData["name"]
 
 	i := newIdentity(h, t)
 	i.RequestInfo.RequestObject = "/api/v1/geolocation/countries/indexer/search"
@@ -139,8 +166,14 @@ func TestCountrySvc_GetDetail(t *testing.T) {
 		t.Errorf("newCountrySvc: %s", err.Error())
 	}
 
+	viper, err := h.GetViper("test-data")
+	if err != nil {
+		t.Errorf("GetViper: %s", err.Error())
+	}
+	testData := viper.GetStringMapString("test-data.geo-location.country.app-layer.service.get-detail")
+
 	req := new(appDTO.GetCountryReqDTO)
-	req.Code = "ID"
+	req.Code = testData["code"]
 
 	i := newIdentity(h, t)
 	i.RequestInfo.RequestObject = "/api/v1/geolocation/country/*"
@@ -163,12 +196,18 @@ func TestCountrySvc_Add(t *testing.T) {
 		t.Errorf("newCountrySvc: %s", err.Error())
 	}
 
+	viper, err := h.GetViper("test-data")
+	if err != nil {
+		t.Errorf("GetViper: %s", err.Error())
+	}
+	testData := viper.GetStringMapString("test-data.geo-location.country.app-layer.service.add")
+
 	req := new(appDTO.AddCountryReqDTO)
-	req.Code = "XX"
-	req.Name = "XX COUNTRY"
-	req.ISO2Code = "XX"
-	req.ISO3Code = ""
-	req.WHORegion = "WPRO"
+	req.Code = testData["code"]
+	req.Name = testData["name"]
+	req.ISO2Code = testData["iso2-code"]
+	req.ISO3Code = testData["iso3-code"]
+	req.WHORegion = testData["who-region"]
 
 	i := newIdentity(h, t)
 	i.RequestInfo.RequestObject = "/api/v1/geolocation/country"
@@ -181,6 +220,14 @@ func TestCountrySvc_Add(t *testing.T) {
 
 	if resp != nil {
 		respJSON := resp.ToJSON()
+		// save to test-data
+		// save result for next test
+		viper.Set("test-data.geo-location.country.app-layer.service.update.id", resp.Data.ID)
+		viper.Set("test-data.geo-location.country.app-layer.service.update.code", resp.Data.Code)
+		viper.Set("test-data.geo-location.country.app-layer.service.delete.code", resp.Data.Code)
+		if err := viper.WriteConfig(); err != nil {
+			t.Errorf("Error: viper.WriteConfig(), %s", err.Error())
+		}
 		t.Logf("Resp: %s", string(respJSON))
 	}
 }
@@ -191,13 +238,19 @@ func TestCountrySvc_Update(t *testing.T) {
 		t.Errorf("newCountrySvc: %s", err.Error())
 	}
 
+	viper, err := h.GetViper("test-data")
+	if err != nil {
+		t.Errorf("GetViper: %s", err.Error())
+	}
+	testData := viper.GetStringMapString("test-data.geo-location.country.app-layer.service.update")
+
 	req := new(appDTO.UpdateCountryReqDTO)
-	req.Keys = &appDTO.UpdateCountryKeysDTO{Code: "XX"}
+	req.Keys = &appDTO.UpdateCountryKeysDTO{Code: testData["code"]}
 	req.Data = &appDTO.UpdateCountryDataDTO{
-		Name:      "XX COUNTRY UPDATED",
-		ISO2Code:  "XX",
-		ISO3Code:  "",
-		WHORegion: "WPRO",
+		Name:      testData["name"],
+		ISO2Code:  testData["iso2-code"],
+		ISO3Code:  testData["iso3-code"],
+		WHORegion: testData["who-region"],
 	}
 
 	i := newIdentity(h, t)
@@ -221,8 +274,14 @@ func TestCountrySvc_Delete(t *testing.T) {
 		t.Errorf("newCountrySvc: %s", err.Error())
 	}
 
+	viper, err := h.GetViper("test-data")
+	if err != nil {
+		t.Errorf("GetViper: %s", err.Error())
+	}
+	testData := viper.GetStringMapString("test-data.geo-location.country.app-layer.service.delete")
+
 	req := new(appDTO.DeleteCountryReqDTO)
-	req.Code = "XX"
+	req.Code = testData["code"]
 
 	i := newIdentity(h, t)
 	i.RequestInfo.RequestObject = "/api/v1/geolocation/country/*"
